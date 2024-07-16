15253f59dc7a874cb8bec21b6bc7a1be27125342
Adarshdev <adarshdev9349@gmail.com>
2024-07-16 14:12:54 +0530

    Minor Changes

--- src/Components/loader.jsx
+++ src/Components/loader.jsx
@@ -1,19 +1,18 @@
-
-
 import React from "react";
-import { reuleaux } from "ldrs";
-reuleaux.register();
+import { metronome } from "ldrs";
+
+metronome.register();
 const Loading = () => {
   return (
-    <div className=" h-screen flex justify-center items-center" style={{ backgroundColor: "#020b12" }}>
-      <l-reuleaux
+    <div
+      className=" h-screen flex justify-center items-center"
+      style={{ backgroundColor: "#020b12" }}
+    >
+      <l-metronome
         size="55"
-        stroke="6"
-        stroke-length="0.15"
-        bg-opacity="0.3"
         speed="1.2"
         color="rgb(226, 146, 231)"
-      ></l-reuleaux>
+      ></l-metronome>
     </div>
   );
 };
