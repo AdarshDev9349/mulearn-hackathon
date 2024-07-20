c31943f1e39afab9c594cf1993aa0c84edb3bfb4
Adarshdev <adarshdev9349@gmail.com>
2024-07-20 21:29:20 +0530

    minor changes

--- src/Components/about.jsx
+++ src/Components/about.jsx
@@ -1,32 +1,85 @@
-import React from "react";
-import mulearnImage from "./images/permute.png";
-import hackathonImage from "./images/example.jpg";
+import { motion, useAnimation } from 'framer-motion';
+import { useInView } from 'react-intersection-observer';
+import React, { useEffect, useState } from 'react';
+import mulearnImage from './images/permute.png';
+import hackathonImage from './images/example.jpg';
+
+const fadeInUp = {
+  hidden: { opacity: 0, y: 50 },
+  visible: { opacity: 1, y: 0 }
+};
+
+const fadeIn = {
+  hidden: { opacity: 0 },
+  visible: { opacity: 1 }
+};
+
+
 
 export default function About() {
+  const controls1 = useAnimation();
+  const [ref1, inView1] = useInView();
+  const [hasAnimated1, setHasAnimated1] = useState(false);
+
+  const controls2 = useAnimation();
+  const [ref2, inView2] = useInView();
+  const [hasAnimated2, setHasAnimated2] = useState(false);
+
+  useEffect(() => {
+    if (inView1 && !hasAnimated1) {
+      controls1.start('visible');
+      setHasAnimated1(true);
+    }
+  }, [controls1, inView1, hasAnimated1]);
+
+  useEffect(() => {
+    if (inView2 && !hasAnimated2) {
+      controls2.start('visible');
+      setHasAnimated2(true);
+    }
+  }, [controls2, inView2, hasAnimated2]);
+
   return (
     <div
       className="py-16 relative about"
       id="about"
-      style={{ backgroundColor: "#020b12" }}
+      style={{ backgroundColor: '#020b12' }}
     >
       <div className="container mx-auto px-6 lg:px-8">
         <div className="absolute inset-0 flex justify-center items-center z-0">
-          <h1 className="custom-heading">ABOUT</h1>
+          <motion.h1
+            className="custom-heading"
+            initial="hidden"
+            animate={controls1}
+            variants={fadeInUp}
+            transition={{ duration: 1 }}
+          >
+            ABOUT
+          </motion.h1>
         </div>
 
         <div className="relative z-10">
           <div className="lg:flex lg:items-center lg:space-x-8 mb-16">
             <div className="lg:flex-1 lg:w-1/2">
               <div className="lg:ml-12">
-                <h2
+                <motion.h2
                   className="text-3xl font-bold mb-4"
-                  style={{ color: "#E292E7" }}
+                  style={{ color: '#E292E7' }}
+                  ref={ref1}
+                  initial="hidden"
+                  animate={controls1}
+                  variants={fadeInUp}
+                  transition={{ duration: 1, delay: 1 }}
                 >
                   About MuLearn UCEK
-                </h2>
-                <p
+                </motion.h2>
+                <motion.p
                   className="lg:text-lg text-m text-justify"
-                  style={{ color: "#EEE8DA" }}
+                  style={{ color: '#EEE8DA' }}
+                  initial="hidden"
+                  animate={controls1}
+                  variants={fadeInUp}
+                  transition={{ duration: 1, delay: 1 }}
                 >
                   Welcome to MuLearn UCEK, your gateway to transformative
                   education. At MuLearn, we are dedicated to revolutionizing
@@ -39,14 +92,18 @@ export default function About() {
                   promote holistic development and a well-rounded educational
                   experience. Join us to redefine education and create a
                   brighter tomorrow.
-                </p>
+                </motion.p>
               </div>
             </div>
             <div className="lg:flex-1 lg:w-1/2 mt-8 lg:mt-0 flex justify-center">
-              <img
-                className=" h-80 object-cover rounded-lg shadow-md"
+              <motion.img
+                className="h-80 object-cover rounded-lg shadow-md"
                 src={mulearnImage}
                 alt="MuLearn UCEK"
+                initial="hidden"
+                animate={controls1}
+                variants={fadeIn}
+                transition={{ duration: 1, delay: 1 }}
               />
             </div>
           </div>
@@ -54,15 +111,24 @@ export default function About() {
           <div className="lg:flex lg:items-center lg:space-x-8">
             <div className="lg:flex-1 lg:w-1/2 lg:order-2">
               <div className="lg:ml-24">
-                <h2
+                <motion.h2
                   className="text-3xl font-bold mb-4"
-                  style={{ color: "#E292E7" }}
+                  style={{ color: '#E292E7' }}
+                  ref={ref2}
+                  initial="hidden"
+                  animate={controls2}
+                  variants={fadeInUp}
+                  transition={{ duration: 1, delay: 1 }}
                 >
                   About the Hackathon
-                </h2>
-                <p
+                </motion.h2>
+                <motion.p
                   className="lg:text-lg text-m text-justify"
-                  style={{ color: "#EEE8DA" }}
+                  style={{ color: '#EEE8DA' }}
+                  initial="hidden"
+                  animate={controls2}
+                  variants={fadeInUp}
+                  transition={{ duration: 1, delay: 1 }}
                 >
                   Welcome to Error 418, the inaugural 36-hour hackathon hosted
                   by MuLearn UCEK. Building on our successful events, Error 418
@@ -75,14 +141,18 @@ export default function About() {
                   innovation. Join us and be part of a transformative journey
                   where creativity and technology converge. Help shape the
                   future of innovation at Error 418 Hackathon.
-                </p>
+                </motion.p>
               </div>
             </div>
             <div className="lg:flex-1 lg:w-1/2 mt-12 lg:mt-0 lg:order-1 flex justify-center">
-              <img
-                className=" h-80 object-cover rounded-lg shadow-md"
+              <motion.img
+                className="h-80 object-cover rounded-lg shadow-md"
                 src={hackathonImage}
                 alt="Hackathon"
+                initial="hidden"
+                animate={controls2}
+                variants={fadeIn}
+                transition={{ duration: 1, delay: 1 }}
               />
             </div>
           </div>
--- src/Components/landing.jsx
+++ src/Components/landing.jsx
@@ -1,24 +1,63 @@
+import { motion } from 'framer-motion';
 import Timer from "./timer";
 import ScrollToBottom from "./arrow";
-import logo from "./images/logo.svg"
+import logo from "./images/logo.svg";
+
+const fadeInUp = {
+  hidden: { opacity: 0, y: 50 },
+  visible: { opacity: 1, y: 0 }
+};
+
+const fadeIn = {
+  hidden: { opacity: 0 },
+  visible: { opacity: 1 }
+};
+
 export default function Landing() {
   return (
     <div id='home' className="relative isolate px-6 pt-24 lg:px-8 lg:pt-12 lg:h-screen h-[90vh]">
-    
       <div className="absolute inset-0 -z-10 overflow-hidden">
-       
+        {/* Background animation or effects */}
       </div>
 
       <div className="mx-auto max-w-2xl py-17 sm:py-28 lg:py-34">
-        <div className="text-center lg:mt-10 mt-20" style={{display:"flex",flexDirection:"column"}}>
-          
-          <img className=" lg:w-5/6 w-full self-center" src={logo} alt="" />
-          <p className="mt-3 lg:text-xl leading-8" style={{color: "#EEE8DA"}}>
+        <div className="text-center lg:mt-10 mt-20" style={{ display: "flex", flexDirection: "column" }}>
+          <motion.img 
+            className="lg:w-5/6 w-full self-center" 
+            src={logo} 
+            alt="" 
+            initial="hidden" 
+            animate="visible" 
+            variants={fadeInUp} 
+            transition={{ duration: 1 }}
+          />
+          <motion.p 
+            className="mt-3 lg:text-xl leading-8" 
+            style={{ color: "#EEE8DA" }}
+            initial="hidden" 
+            animate="visible" 
+            variants={fadeInUp} 
+            transition={{ duration: 1, delay: 0.5 }}
+          >
             Hack a way to brew coffee in your teapot
-          </p>
-          <div className="flex justify-center pt-2"><Timer/></div>
-           
-          <div className="mt-10 flex items-center justify-center gap-x-6">
+          </motion.p>
+          <motion.div 
+            className="flex justify-center pt-2"
+            initial="hidden" 
+            animate="visible" 
+            variants={fadeIn} 
+            transition={{ duration: 1, delay: 1 }}
+          >
+            <Timer />
+          </motion.div>
+
+          <motion.div 
+            className="mt-10 flex items-center justify-center gap-x-6"
+            initial="hidden" 
+            animate="visible" 
+            variants={fadeIn} 
+            transition={{ duration: 1, delay: 1.5 }}
+          >
             <a
               href="#register"
               className="rounded-md bg-transparent border-2 px-3.5 py-2.5 lg:text-lg font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
@@ -27,20 +66,20 @@ export default function Landing() {
             </a>
             <a
               href="#sponsor"
-              
-              className="rounded-md bg-white text-black border-2 px-3.5 py-2.5 lg:text-lg  font-semibold  shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+              className="rounded-md bg-white text-black border-2 px-3.5 py-2.5 lg:text-lg font-semibold shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
             >
-            <a href="mailto:mulearnucek@gmail.com"> Contact Us</a>
-             
+              <a href="mailto:mulearnucek@gmail.com">Contact Us</a>
             </a>
-          </div>
+          </motion.div>
         </div>
       </div>
       <div
         aria-hidden="true"
         className="absolute inset-x-0 top-[calc(100%-13rem)] -z-10 transform-gpu overflow-hidden blur-3xl sm:top-[calc(100%-30rem)]"
-      ></div>
-      <ScrollToBottom/>
+      >
+        {/* Background animation or effects */}
+      </div>
+      <ScrollToBottom />
     </div>
   );
 }
